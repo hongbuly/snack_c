ac9cf794fbe0eb69ebd4b8710cf918261a44cf5b
hongbuly <hongbuly@naver.com>
2022-05-28 11:18:57 +0900

    Modifed main, testfile
    키보드 입력을 받아서 뱀이 움직임
    sleep 함수가 소수점이 안되고, 뱀 꼬리가 사라지기도 함

--- main.cpp
+++ main.cpp
@@ -4,9 +4,18 @@
 //#pragma comment(lib, "./pdcurses.lib")
 #include <clocale>
 #include <ncurses.h>
+#include <pthread.h>
+#include <unistd.h>
 using namespace std;
 
 int map[21][21];
+int direction_x = -1; //left:-1, right:1
+int direction_y; //up:-1, down:1
+int head_y = 10;
+int head_x = 9;
+int tail_x[400];
+int tail_y[400];
+int tail_length = 2;
 
 void setMap() {
 	for (int i = 0; i < 21; i++) {
@@ -24,19 +33,20 @@ void setMap() {
 	map[20][0] = 2;
 	map[0][20] = 2;
 
-	map[10][9] = 3;
-	map[10][10] = 4;
-	map[10][11] = 4;
+	tail_y[0] = 10;
+	tail_x[0] = 10;
+	tail_y[1] = 10;
+	tail_x[1] = 11;
 }
 
 void drawMap() {
+	clear();
+	int tail_position = 0;
 	for (int i = 0; i < 21; i++) {
 		for (int j = 0; j < 21; j++) {
 			if (map[i][j] == 1 || map[i][j] == 2)
 				mvprintw(i, j, "\u2B1B");
-			else if (map[i][j] == 0)
-				mvprintw(i, j, "\u2B1C");
-			else if (map[i][j] == 3) {
+			else if (head_y == i && head_x == j) {
 				start_color();
 				init_pair(1, COLOR_YELLOW, COLOR_BLACK);
 
@@ -44,7 +54,8 @@ void drawMap() {
 				mvprintw(i, j, "\u2B1B");
 				attroff(COLOR_PAIR(1));
 			}
-			else if (map[i][j] == 4) {
+			else if (tail_position < tail_length && tail_y[tail_position] == i && tail_x[tail_position] == j) {
+				tail_position++;
 				start_color();
 				init_pair(2, COLOR_MAGENTA, COLOR_BLACK);
 
@@ -52,10 +63,61 @@ void drawMap() {
 				mvprintw(i, j, "\u2B1B");
 				attroff(COLOR_PAIR(2));
 			}
-
+			else if (map[i][j] == 0) {
+				mvprintw(i, j, "\u2B1C");
+			}
 		}
 	}
 	refresh();
+}
+
+bool isGameOver() {
+	if (head_x == 0 || head_x == 20 || head_y == 0 || head_y == 20) {
+		return true;
+	}
+	return false;
+}
+
+void moveSnake() {
+	int prevX = head_x;
+	int prevY = head_y;
+
+	head_x += direction_x;
+	head_y += direction_y;
+
+	for (int i = tail_length - 1; i > 0; i--) {
+		tail_x[i] = tail_x[i - 1];
+		tail_y[i] = tail_y[i - 1];
+	}
+	tail_x[0] = prevX;
+	tail_y[0] = prevY;
+}
+
+void* getInput(void *arg) {
+	pthread_t tid;
+	tid = pthread_self();
+	keypad(stdscr, TRUE);
+
+	while (!isGameOver()) {
+		int input = getch();
+		//72 up, 80 down, 77 right, 75 left
+		if (input == KEY_UP) {
+			direction_y = -1;
+			direction_x = 0;
+		}
+		else if (input == KEY_DOWN) {
+			direction_y = 1;
+			direction_x = 0;
+		}
+		else if (input == KEY_RIGHT) {
+			direction_x = 1;
+			direction_y = 0;
+		}
+		else if (input == KEY_LEFT) {
+			direction_x = -1;
+			direction_y = 0;
+		}
+	}
 }
 
 int main() {
@@ -64,13 +126,20 @@ int main() {
 	char userName[8];
 
 	initscr();
-	setMap();
-	drawMap();
-	
-	// keypad(stdscr, TRUE);
 	curs_set(0);
 	noecho();
-	// scanw("%s", userName);
+
+	setMap();
+	pthread_t thread;
+	int thr_id = pthread_create(&thread, NULL, getInput, NULL);
+
+	while (!isGameOver()) {
+		drawMap();
+		sleep(1);
+		moveSnake();
+	}
+
+	mvprintw(22, 5, "GameOver");
 	
 	getch();
 	endwin();
